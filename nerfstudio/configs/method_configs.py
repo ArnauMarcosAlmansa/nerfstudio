d04c3b9f59f4614373bef6dc0c9ac937c3097a9d
Arnau Marcos Almansa <arnaumarcosalmansa@gmail.com>
2024-05-29 18:35:23 +0200

    entrenar con 4 bandas

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -24,7 +24,7 @@ from typing import Dict, Union
 import tyro
 
 from nerfstudio.cameras.camera_optimizers import CameraOptimizerConfig
-from nerfstudio.configs.base_config import ViewerConfig
+from nerfstudio.configs.base_config import ViewerConfig, LoggingConfig, LocalWriterConfig
 from nerfstudio.configs.external_methods import ExternalMethodDummyTrainerConfig, get_external_methods
 from nerfstudio.data.datamanagers.base_datamanager import VanillaDataManager, VanillaDataManagerConfig
 from nerfstudio.data.datamanagers.full_images_datamanager import FullImageDatamanagerConfig
@@ -58,6 +58,7 @@ from nerfstudio.models.mipnerf import MipNerfModel
 from nerfstudio.models.nerfacto import NerfactoModelConfig
 from nerfstudio.models.neus import NeuSModelConfig
 from nerfstudio.models.neus_facto import NeuSFactoModelConfig
+from nerfstudio.models.polarization import PolarizationModelConfig
 from nerfstudio.models.semantic_nerfw import SemanticNerfWModelConfig
 from nerfstudio.models.splatfacto import SplatfactoModelConfig
 from nerfstudio.models.tensorf import TensoRFModelConfig
@@ -65,6 +66,7 @@ from nerfstudio.models.vanilla_nerf import NeRFModel, VanillaModelConfig
 from nerfstudio.pipelines.base_pipeline import VanillaPipelineConfig
 from nerfstudio.pipelines.dynamic_batch import DynamicBatchPipelineConfig
 from nerfstudio.plugins.registry import discover_methods
+from nerfstudio.utils import writer
 
 method_configs: Dict[str, Union[TrainerConfig, ExternalMethodDummyTrainerConfig]] = {}
 descriptions = {
@@ -710,7 +712,7 @@ method_configs["polarization"] = TrainerConfig(
             train_num_rays_per_batch=4096,
             eval_num_rays_per_batch=4096,
         ),
-        model=NerfactoModelConfig(
+        model=PolarizationModelConfig(
             eval_num_rays_per_chunk=1 << 15,
             average_init_density=0.01,
             camera_optimizer=CameraOptimizerConfig(mode="SO3xR3"),
@@ -732,6 +734,17 @@ method_configs["polarization"] = TrainerConfig(
     },
     viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
     vis="viewer",
+    logging=LoggingConfig(
+        local_writer=LocalWriterConfig(enable=True, stats_to_track=(
+            writer.EventName.ITER_TRAIN_TIME,
+            writer.EventName.TRAIN_RAYS_PER_SEC,
+            writer.EventName.CURR_TEST_PSNR,
+            writer.EventName.VIS_RAYS_PER_SEC,
+            writer.EventName.TEST_RAYS_PER_SEC,
+            writer.EventName.ETA,
+            writer.EventName.TRAIN_LOSS
+        ))
+    )
 )
 
 
--- nerfstudio/model_components/renderers.py
+++ nerfstudio/model_components/renderers.py
@@ -229,6 +229,173 @@ class RGBRenderer(nn.Module):
             torch.clamp_(rgb, min=0.0, max=1.0)
         return rgb
 
+class NChannelRenderer(nn.Module):
+    """Standard volumetric rendering.
+
+    Args:
+        background_color: Background color as RGB. Uses random colors if None.
+    """
+
+    def __init__(self, background_color: BackgroundColor = "random") -> None:
+        super().__init__()
+        self.background_color: BackgroundColor = background_color
+
+    @classmethod
+    def combine_rgb(
+            cls,
+            channels: Float[Tensor, "*bs num_samples N"],
+            weights: Float[Tensor, "*bs num_samples 1"],
+            background_color: BackgroundColor = "random",
+            ray_indices: Optional[Int[Tensor, "num_samples"]] = None,
+            num_rays: Optional[int] = None,
+    ) -> Float[Tensor, "*bs 3"]:
+        """Composite samples along ray and render color image.
+        If background color is random, no BG color is added - as if the background was black!
+
+        Args:
+            rgb: RGB for each sample
+            weights: Weights for each sample
+            background_color: Background color as RGB.
+            ray_indices: Ray index for each sample, used when samples are packed.
+            num_rays: Number of rays, used when samples are packed.
+
+        Returns:
+            Outputs rgb values.
+        """
+        if ray_indices is not None and num_rays is not None:
+            # Necessary for packed samples from volumetric ray sampler
+            if background_color == "last_sample":
+                raise NotImplementedError("Background color 'last_sample' not implemented for packed samples.")
+            comp_rgb = nerfacc.accumulate_along_rays(
+                weights[..., 0], values=channels, ray_indices=ray_indices, n_rays=num_rays
+            )
+            accumulated_weight = nerfacc.accumulate_along_rays(
+                weights[..., 0], values=None, ray_indices=ray_indices, n_rays=num_rays
+            )
+        else:
+            comp_rgb = torch.sum(weights * channels, dim=-2)
+            accumulated_weight = torch.sum(weights, dim=-2)
+        if BACKGROUND_COLOR_OVERRIDE is not None:
+            background_color = BACKGROUND_COLOR_OVERRIDE
+        if background_color == "random":
+            # If background color is random, the predicted color is returned without blending,
+            # as if the background color was black.
+            return comp_rgb
+        elif background_color == "last_sample":
+            # Note, this is only supported for non-packed samples.
+            background_color = channels[..., -1, :]
+        background_color = cls.get_background_color(background_color, shape=comp_rgb.shape, device=comp_rgb.device)
+
+        assert isinstance(background_color, torch.Tensor)
+        comp_rgb = comp_rgb + background_color * (1.0 - accumulated_weight)
+        return comp_rgb
+
+    @classmethod
+    def get_background_color(
+            cls, background_color: BackgroundColor, shape: Tuple[int, ...], device: torch.device
+    ) -> Union[Float[Tensor, "N"], Float[Tensor, "*bs N"]]:
+        """Returns the RGB background color for a specified background color.
+        Note:
+            This function CANNOT be called for background_color being either "last_sample" or "random".
+
+        Args:
+            background_color: The background color specification. If a string is provided, it must be a valid color name.
+            shape: Shape of the output tensor.
+            device: Device on which to create the tensor.
+
+        Returns:
+            Background color as RGB.
+        """
+        assert background_color not in {"last_sample", "random"}
+        assert shape[-1] > 0, "Background color must hava at least a band."
+        if BACKGROUND_COLOR_OVERRIDE is not None:
+            background_color = BACKGROUND_COLOR_OVERRIDE
+        if isinstance(background_color, str) and background_color in colors.COLORS_DICT:
+            background_color = colors.COLORS_DICT[background_color]
+        assert isinstance(background_color, Tensor)
+
+        # Ensure correct shape
+        return background_color.expand(shape).to(device)
+
+    def blend_background(
+            self,
+            image: Tensor,
+            background_color: Optional[BackgroundColor] = None,
+    ) -> Float[Tensor, "*bs 3"]:
+        """Blends the background color into the image if image is RGBA.
+        Otherwise no blending is performed (we assume opacity of 1).
+
+        Args:
+            image: RGB/RGBA per pixel.
+            opacity: Alpha opacity per pixel.
+            background_color: Background color.
+
+        Returns:
+            Blended RGB.
+        """
+
+        # No blending to be made, since N channel images don't have alpha
+
+        return image
+
+    def blend_background_for_loss_computation(
+            self,
+            pred_image: Tensor,
+            pred_accumulation: Tensor,
+            gt_image: Tensor,
+    ) -> Tuple[Tensor, Tensor]:
+        """Blends a background color into the ground truth and predicted image for
+        loss computation.
+
+        Args:
+            gt_image: The ground truth image.
+            pred_image: The predicted RGB values (without background blending).
+            pred_accumulation: The predicted opacity/ accumulation.
+        Returns:
+            A tuple of the predicted and ground truth RGB values.
+        """
+        background_color = self.background_color
+        if background_color == "last_sample":
+            background_color = "black"  # No background blending for GT
+        elif background_color == "random":
+            background_color = torch.rand_like(pred_image)
+            pred_image = pred_image + background_color * (1.0 - pred_accumulation)
+        gt_image = self.blend_background(gt_image, background_color=background_color)
+        return pred_image, gt_image
+
+    def forward(
+            self,
+            rgb: Float[Tensor, "*bs num_samples 3"],
+            weights: Float[Tensor, "*bs num_samples 1"],
+            ray_indices: Optional[Int[Tensor, "num_samples"]] = None,
+            num_rays: Optional[int] = None,
+            background_color: Optional[BackgroundColor] = None,
+    ) -> Float[Tensor, "*bs 3"]:
+        """Composite samples along ray and render color image
+
+        Args:
+            rgb: RGB for each sample
+            weights: Weights for each sample
+            ray_indices: Ray index for each sample, used when samples are packed.
+            num_rays: Number of rays, used when samples are packed.
+            background_color: The background color to use for rendering.
+
+        Returns:
+            Outputs of rgb values.
+        """
+
+        if background_color is None:
+            background_color = self.background_color
+
+        if not self.training:
+            rgb = torch.nan_to_num(rgb)
+        rgb = self.combine_rgb(
+            rgb, weights, background_color=background_color, ray_indices=ray_indices, num_rays=num_rays
+        )
+        if not self.training:
+            torch.clamp_(rgb, min=0.0, max=1.0)
+        return rgb
+
 
 class SHRenderer(nn.Module):
     """Render RGB value from spherical harmonics.
